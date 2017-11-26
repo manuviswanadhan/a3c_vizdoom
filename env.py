0305f04c7e83aef4cd886b54e9cc26b819d81491
manuviswanadhan <manu.viswanadhan@gmail.com>
2017-11-25 20:25:20 -0600

    initial commit

---
+++ env.py
@@ -0,0 +1,16 @@
+from vizdoom import *
+
+def make_env(worker_id, config_file_path=None):
+    print("Initializing doom environment", worker_id, "...")
+    game = DoomGame()
+    game.load_config(config_file_path)
+    game.init()
+    return game
+
+def make_visual_env(config_file_path=None):
+    print("Initializing doom environment...")
+    game = DoomGame()
+    game.load_config(config_file_path)
+    game.set_window_visible(True)
+    game.init()
+    return game
